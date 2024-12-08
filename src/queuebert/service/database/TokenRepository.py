be0135b6e30e1f18c2cc79f88b9b5078d88fd34c
Daniel Törbacka <daniel.torbacka@gmail.com>
2024-12-08 11:54:07 +0100

    Add logging and excpetion handling for store token

--- src/queuebert/service/database/TokenRepository.py
+++ src/queuebert/service/database/TokenRepository.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from google.cloud import firestore
@@ -11,7 +12,11 @@ class TokenRepository:
         self.token_collection = firestore.Client(database="queuebert-firestore").collection("tokens")
 
     def store_token(self, team, token_document):
-        self.token_collection.document(team).set(token_document)
+        result = self.token_collection.document(team).set(token_document, merge=True)
+        print(f"Store team token result {json.dumps(result)}")
+        if not result.transform_results:
+            raise Exception(f"Fail to store token inside firestore for team {team}")
+
 
     def get_token(self, team):
         token_document = self.token_collection.document(team).get().to_dict()
